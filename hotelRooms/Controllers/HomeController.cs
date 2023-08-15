c94623f59e92248a3e289c89ab8595580e808de6
Turbobent <kasp728h@edu.mercantec.dk>
2023-08-15 12:26:07 +0200

    update room form

--- hotelRooms/Controllers/HomeController.cs
+++ hotelRooms/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿
 using hotelRooms.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 
@@ -22,8 +23,84 @@ namespace HotelH2.Controllers
         {
             return View();
         }
+        public IActionResult Update(int id)
+        {
 
-     
+            using (SqlConnection con = new SqlConnection("Data Source=PCVDATALAP100\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False"))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT * FROM hoteltest.dbo.Rooms WHERE ID = @ID";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@ID", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Rooms roomToUpdate = new Rooms();
+                                roomToUpdate.ID = (int)reader[0];
+                                roomToUpdate.Type = (string)reader[1];
+                                roomToUpdate.Price = (int)reader[2];
+                                roomToUpdate.occipied = (bool)reader[3];
+                                roomToUpdate.maxPersoncount = (int)reader[4];
+                                roomToUpdate.startdate = (DateOnly)reader[5];
+                                roomToUpdate.slutdate = (DateOnly)reader[6];
+
+                                return View(roomToUpdate);
+                            }
+                            else
+                            {
+                                return NotFound();
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Handle exceptions here
+                    return View("Error"); // Redirect to an error page or show an error view
+                }
+            }
+        }
+
+        [HttpPost]
+        public bool UpdateRoomDetails(Rooms updatedRoom)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=PCVDATALAP100\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False"))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "UPDATE hoteltest.dbo.Rooms SET Type = @Type, Price = @Price, occipied = @Occipied, maxPersoncount = @MaxPersoncount, startdate = @StartDate, slutdate = @EndDate WHERE ID = @ID";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@ID", updatedRoom.ID);
+                        cmd.Parameters.AddWithValue("@Type", updatedRoom.Type);
+                        cmd.Parameters.AddWithValue("@Price", updatedRoom.Price);
+                        cmd.Parameters.AddWithValue("@Occipied", updatedRoom.occipied);
+                        cmd.Parameters.AddWithValue("@MaxPersoncount", updatedRoom.maxPersoncount);
+                        cmd.Parameters.AddWithValue("@StartDate", updatedRoom.startdate);
+                        cmd.Parameters.AddWithValue("@EndDate", updatedRoom.slutdate);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        return rowsAffected > 0;
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Handle exceptions here
+                    return false;
+                }
+            }
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
